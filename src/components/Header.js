5079be5c027a90c89362edcacfba6a7c87a91f69
Victor Yuri <victoryuritc@yahoo.com>
2022-07-28 19:04:03 -0300

    h1 and h3 elements added on Header to show email saved on store and recovered with mapStateToProps, amount of money and currency, respectively. Header rendered at Wallet. Wallet rendered at App.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
+    const money = 0;
+    const currency = 'BRL';
     return (
       <div>
-        s
+        <h1 data-testid="email-field">
+          { email }
+        </h1>
+        <h3 data-testid="total-field">
+          { money }
+        </h3>
+        <h3 data-testid="header-currency-field">
+          { currency }
+        </h3>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
