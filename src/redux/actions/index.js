e2ef3d85b5a6c66e6d500462c67885467306cf67
Victor Yuri <victoryuritc@yahoo.com>
2022-07-28 18:50:37 -0300

    email and passwords inputs created at Login page. enableLoginButton requires min of characters is a valid email. constants folder created to store USER_EMAIL. user reducer created and userLoginAction created.

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+import USER_EMAIL from '../../constants/index';
+
+const userLoginAction = (email) => ({
+  type: USER_EMAIL,
+  email,
+});
+
+export default userLoginAction;
