aed4696d925209c311d21f96aacdf27dc5861432
Victor Yuri <victoryuritc@yahoo.com>
2022-07-29 15:14:49 -0300

    constants actions created at constants folder. expenses reducer built and called at store.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './user';
 import wallet from './wallet';
+import expenses from './expenses';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
-const reducer = combineReducers({ user, wallet });
+const reducer = combineReducers({ user, wallet, expenses });
 
 export default reducer;
