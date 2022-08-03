f4b4c1334c140bce263538d5755e8779bc5654ac
Victor Yuri <victoryuritc@yahoo.com>
2022-08-03 20:11:35 -0300

    comments at WalletForm

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -104,10 +104,10 @@ class WalletForm extends Component {
       />)
     ));
 
-    const { isExpenseBeingEdited } = this.props;
+    /* const { isExpenseBeingEdited } = this.props; */
 
     const SALVAR_DESPESA = 'Salvar despesa';
-    const EDITAR_DESPESA = 'Editar despesa';
+    /* const EDITAR_DESPESA = 'Editar despesa'; */
 
     const {
       currency,
@@ -210,9 +210,10 @@ class WalletForm extends Component {
           name="saveNewExpenseButton"
           onClick={ this.onClickSaveNewExpense }
         >
-          { isExpenseBeingEdited
+          { /* isExpenseBeingEdited
             ? EDITAR_DESPESA
-            : SALVAR_DESPESA }
+            : */ SALVAR_DESPESA
+          }
         </button>
 
       </div>
@@ -242,7 +243,7 @@ WalletForm.propTypes = {
   dispatchTotalValue: PropTypes.func.isRequired,
   exchangeRates: PropTypes.objectOf(PropTypes.object).isRequired,
   expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
-  isExpenseBeingEdited: PropTypes.bool.isRequired,
+  /* isExpenseBeingEdited: PropTypes.bool.isRequired, */
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
